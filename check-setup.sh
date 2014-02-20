bb2d6eb17ba8665c5368363d4268b05d32a759a9
vagrant <vagrant@precise32.(none)>
2014-02-20 02:01:23 +0000

    my lab7

--- check-setup.sh
+++ check-setup.sh
@@ -14,7 +14,7 @@ npm_missing="0"
 cur_lab=7
 
 system=$(uname -a)
-if [ "$system" == "Linux precise32 3.2.0-23-generic-pae #36-Ubuntu SMP Tue Apr 10 22:19:09 UTC 2012 i686 i686 i386 GNU/Linux" ]
+if [ "$system" == "Linux precise32 3.2.0-23-generic-pae #36-Ubuntu SMP Tue Apr 10 22:19:09 UTC 2012 i686 athlon i386 GNU/Linux" ]
 then
   sys_vagrant="1"  
   echo "Running on Vagrant guest"
--- routes/project.js
+++ routes/project.js
@@ -5,6 +5,10 @@ exports.projectInfo = function(req, res) { 
 
   // query for the specific project and
   // call the following callback
+  models.Project
+    .find({ "_id": projectID })
+    .sort('-date')
+    .exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -18,6 +22,19 @@ exports.addProject = function(req, res) {
 
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
+  var newProject = new models.Project({
+    "title": form_data["project_title"],
+    "date": new Date(form_data["date"]),
+    "summary": form_data["summary"],
+    "image": form_data["image_url"]
+  });
+
+  newProject.save(afterSaving);
+
+  function afterSaving(err) {
+    if(err) { console.log(err); res.send(500); }
+    res.redirect('/');
+  }
 }
 
 exports.deleteProject = function(req, res) {
@@ -25,4 +42,13 @@ exports.deleteProject = function(req, res) {
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
+    models.Project
+    .find({ "_id": projectID })
+    .remove()
+    .exec(afterRemove);
+
+  function afterRemove(err, projects) {
+    if(err) console.log(err);
+    res.send("Project removed.");
+  }
 }
